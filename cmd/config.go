ebc42de522563af1e1d1f49f950f9e841c7d6a1a
kangjoa <joannalaikang@gmail.com>
2025-09-28 21:18:25 -0700

    Add templates

---
+++ cmd/config.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage configuration settings",
+	Long:  "Manage configuration settings for the gostart tool.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Configuration management")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
