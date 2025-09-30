64ce6136ccdb34b2400151bc7a9324287feee258
kangjoa <joannalaikang@gmail.com>
2025-09-29 19:18:56 -0700

    Use templates

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 4210 Final Project
 
 Final project description and requirements found [here](https://github.com/Tech-at-DU/ACS-4210-Strongly-Typed-Languages?tab=readme-ov-file#%66%69%6E%61%6C%2D%70%72%6F%6A%65%63%74).
+
+## How to run
+
+```zsh
+go build
+./go-new-project init my-app
+```
--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -12,10 +15,77 @@ var initCmd = &cobra.Command{
 	Long:  "init generates a minimal Go project (go.mod, main.go, README, LICENSE, .gitignore).",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Initializing project: %s\n", args[0])
+		projectName := args[0]
+		fmt.Printf("Initializing project: %s\n", projectName)
+
+		// Create project directory
+		if err := os.Mkdir(projectName, 0755); err != nil {
+			fmt.Printf("Error creating directory: %v\n", err)
+			return
+		}
+
+		// Create files from templates
+		if err := createFromTemplate("templates/go.mod.template",
+			filepath.Join(projectName, "go.mod"), projectName); err != nil {
+			fmt.Printf("Error creating go.mod: %v\n", err)
+			return
+		}
+
+		if err := createFromTemplate("templates/main.go.template",
+			filepath.Join(projectName, "main.go"), projectName); err != nil {
+			fmt.Printf("Error creating main.go: %v\n", err)
+			return
+		}
+
+		if err := createFromTemplate("templates/README.md.template",
+			filepath.Join(projectName, "README.md"), projectName); err != nil {
+			fmt.Printf("Error creating README.md: %v\n", err)
+			return
+		}
+
+		// Copy static template files
+		if err := copyFile("templates/LICENSE", filepath.Join(projectName, "LICENSE")); err != nil {
+			fmt.Printf("Error copying LICENSE: %v\n", err)
+			return
+		}
+
+		if err := copyFile("templates/.gitignore", filepath.Join(projectName, ".gitignore")); err != nil {
+			fmt.Printf("Error copying .gitignore: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Project %s created successfully!\n", projectName)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
+
+func createFromTemplate(templatePath, outputPath, projectName string) error {
+	template, err := os.ReadFile(templatePath)
+	if err != nil {
+		return err
+	}
+
+	content := strings.ReplaceAll(string(template), "{{.ProjectName}}", projectName)
+
+	if err := os.WriteFile(outputPath, []byte(content), 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	sourceFile, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(dst, sourceFile, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
